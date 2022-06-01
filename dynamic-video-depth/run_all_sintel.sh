154563a589964f32cd7252b65c61ee4a33778758
benson1016087 <benson1016087@gmail.com>
2022-06-01 09:41:44 +0800

    Fix bug in run_all_sintel.sh

--- dynamic-video-depth/run_all_sintel.sh
+++ dynamic-video-depth/run_all_sintel.sh
@@ -7,5 +7,5 @@ gpu="$1"
 for track_id in "alley_1" "ambush_5" "bamboo_2" "bandage_1" "cave_2" "market_6" "shaman_2" "sleeping_1" "temple_2" 
 do
     bash ./experiments/davis/train_sequence.sh ${gpu} --track_id ${track_id} --path_suffix _sintel \
-    --backbone robust_CVD/Sintel/googckpt_3epoch/${track_id}/R0-100_hierarchical2_midas2/StD100.0_StR1.0_SmD0_SmR0.0/checkpoints/0003.pt
+    --backbone robust_CVD/Sintel/googckpt_3epoch/${track_id}/R0-100_hierarchical2_midas2/StD100.0_StR1.0_SmD0_SmR0.0/checkpoints/0003.pth
 done
