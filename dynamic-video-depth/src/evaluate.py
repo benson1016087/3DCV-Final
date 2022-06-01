1380dcb24bcd7672c11a743eab87a21885a600cf
PingChiaHuang <r10922095@ntu.edu.tw>
2022-06-01 21:00:58 +0800

    add mask

--- dynamic-video-depth/src/evaluate.py
+++ dynamic-video-depth/src/evaluate.py
@@ -16,39 +16,60 @@ def parse() -> Namespace:
     return parser.parse_args()
 
 
-def get_metrics(preds, gts, acc_thresholds=[1.25, 1.25**2, 1.25**3]):
+def get_metrics(preds, gts, valids=None, acc_thresholds=[1.25, 1.25**2, 1.25**3]):
+    if valids is None:
+        valids = np.ones_like(gts) > 0
+
     preds = np.array(preds).reshape(len(preds), -1)
     gts = np.array(gts).reshape(len(gts), -1)
+    valids = np.array(valids).reshape(len(valids), -1)
     print(gts.min(), gts.max())
     print(preds.min(), preds.max())
 
-    RMSE = np.sqrt(((preds - gts) ** 2).mean(axis=-1)).mean()
-    LRMSE = np.sqrt(((np.log(preds) - np.log(gts)) ** 2).mean(axis=-1)).mean()
-    abs_rel = (np.abs(preds - gts) / gts).mean()
-    square_rel = (np.square(preds - gts) / gts).mean()
+    RMSE, LRMSE, abs_rel, square_rel = [], [], [], []
+    for pred, gt, valid in zip(preds, gts, valids):
+        pred, gt = pred[valid], gt[valid]
+        RMSE.append(np.sqrt(((pred - gt) ** 2).mean()))
+        LRMSE.append(np.sqrt(((np.log(pred) - np.log(gt)) ** 2).mean()))
+        abs_rel.append((np.abs(pred - gt) / gt).mean())
+        square_rel.append((np.square(pred - gt) / gt).mean())
 
-    res = {"RMSE": RMSE, "Log RMSE": LRMSE, "Abs Rel": abs_rel, "Sq Rel": square_rel}
+    res = {
+        "RMSE": np.mean(RMSE),
+        "Log RMSE": np.mean(LRMSE),
+        "Abs Rel": np.mean(abs_rel),
+        "Sq Rel": np.mean(square_rel),
+    }
 
     for acc_threshold in acc_thresholds:
-        acc = (np.maximum(gts / (preds + 1e-6), preds / (gts + 1e-6)) < acc_threshold).mean()
-        res[f"acc-{acc_threshold}"] = acc
+        acc = []
+        for pred, gt, valid in zip(preds, gts, valids):
+            pred, gt = pred[valid], gt[valid]
+            acc.append((np.maximum(gt / (pred + 1e-6), pred / (gt + 1e-6)) < acc_threshold).mean())
+        res[f"acc-{acc_threshold}"] = np.mean(acc)
 
     return res
 
+
 def get_one_video_metrics(pred_dir, gt_dir):
+    clip_val = 80
+
     n = len(list(gt_dir.iterdir()))
-    preds, gts = [], []
+    preds, gts, valids = [], [], []
     for i in range(n):
         pred_file = np.load(f"{pred_dir}/batch{i:04d}.npz")
         gt_file = np.load(f"{gt_dir}/frame_{i:05d}.npz")
 
-        pred_depth = pred_file["depth"]
-        gt_depth = resize(gt_file["depth"][..., None], (160, 384))
+        valid = resize(gt_file["invalid"][..., None], (160, 384)) == 0
+        gt_depth = resize(np.minimum(gt_file["depth"], clip_val)[..., None], (160, 384))
+        pred_depth = np.minimum(pred_file["depth"], clip_val)
 
         preds.append(pred_depth)
         gts.append(gt_depth)
-    
-    return get_metrics(preds, gts)
+        valids.append(valid)
+
+    return get_metrics(preds, gts, valids)
+
 
 if __name__ == "__main__":
     args = parse()
@@ -56,7 +77,17 @@ if __name__ == "__main__":
         metrics = get_one_video_metrics(args.input_dir_1, args.input_dir_2)
         print(metrics)
     else:
-        val_list = ["alley_1", "ambush_5", "bamboo_2", "bandage_1", "cave_2", "market_6", "shaman_2", "sleeping_1", "temple_2"]
+        val_list = [
+            "alley_1",
+            "ambush_5",
+            "bamboo_2",
+            "bandage_1",
+            "cave_2",
+            "market_6",
+            "shaman_2",
+            "sleeping_1",
+            "temple_2",
+        ]
 
         res = [track_id for track_id in val_list if track_id in str(args.input_dir_1)]
         assert len(res) == 1
@@ -71,5 +102,3 @@ if __name__ == "__main__":
             print(track_id, ret)
         metrics_rec = {k: sum(v) / len(v) for k, v in metrics_rec.items()}
         print(f"Among all tracks: {metrics_rec}")
-
-    
