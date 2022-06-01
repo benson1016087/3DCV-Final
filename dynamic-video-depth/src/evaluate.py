6ff884c5461b8af36e5e516947df330533203615
PingChiaHuang <r10922095@ntu.edu.tw>
2022-06-01 10:20:06 +0800

    add more metrics

--- dynamic-video-depth/src/evaluate.py
+++ dynamic-video-depth/src/evaluate.py
@@ -2,27 +2,35 @@ from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
 import numpy as np
-import torch
-
 from resize import resize
 
 
-def parse():
+def parse() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument("input_dir_1", type=Path)
     parser.add_argument("input_dir_2", type=Path)
 
     return parser.parse_args()
 
-def matrics(preds, gts):
+
+def matrics(preds, gts, acc_thresholds=[1.25, 1.25**2, 1.25**3]):
     preds = np.array(preds).reshape(len(preds), -1)
     gts = np.array(gts).reshape(len(gts), -1)
     print(gts.min())
-    
+
     RMSE = np.sqrt(((preds - gts) ** 2).mean(axis=-1)).mean()
     LRMSE = np.sqrt(((np.log(preds) - np.log(gts)) ** 2).mean(axis=-1)).mean()
-    
-    return {"RMSE": RMSE, "Log RMSE": LRMSE}
+    abs_rel = (np.abs(preds - gts) / gts).mean()
+    square_rel = (np.square(preds - gts) / gts).mean()
+
+    res = {"RMSE": RMSE, "Log RMSE": LRMSE, "Abs Rel": abs_rel, "Sq Rel": square_rel}
+
+    for acc_threshold in acc_thresholds:
+        acc = (np.maximum(gts / (preds + 1e-6), preds / (gts + 1e-6)) < acc_threshold).mean()
+        res[f"acc-{acc_threshold}"] = acc
+
+    return res
+
 
 if __name__ == "__main__":
     args = parse()
@@ -32,12 +40,11 @@ if __name__ == "__main__":
     for i in range(n):
         pred_file = np.load(f"{args.input_dir_1}/batch{i:04d}.npz")
         gt_file = np.load(f"{args.input_dir_2}/frame_{i:05d}.npz")
-        
+
         pred_depth = pred_file["depth"]
         gt_depth = resize(gt_file["depth"][..., None], (160, 384))
 
         preds.append(pred_depth)
         gts.append(gt_depth)
-    
+
     print(matrics(preds, gts))
-        
